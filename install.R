d7a255966f72f5acfa6c2ce8de3cfa298693d0d0
ani ruhil <ruhil@ohio.edu>
2020-02-25 19:17:44 -0500

    first commit to gh repo

---
+++ install.R
@@ -0,0 +1,16 @@
+install.packages("tidyverse")
+install.packages("rmarkdown")
+install.packages("httr")
+install.packages("shinydashboard")
+install.packages('leaflet')
+install.packages('knitr')
+install.packages('DT')
+install.packages('readxl')
+install.packages('GGally')
+install.packages('foreign')
+install.packages('kableExtra')
+
+
+
+
+
